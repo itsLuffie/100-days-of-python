a1c5645c824606763e94f800e813bd23671f6838
Pawan Chaudhary <devrazzchaudhary@proton.me>
2024-04-20 12:47:50 +0545

    random.choice function learned

--- day4/day4.2.py
+++ day4/day4.2.py
@@ -2,5 +2,7 @@ name_string = input("Enter everyones name: ")
 name = name_string.split(", ")
 
 import random
-x = random.randint(0,len(name))
-print (f"The person paying bill will be {name[x]}.")
+x = random.choice(name)         #choice function will directly pick the name from the function
+print(f"{x} will pay")
+# paying_person = random.randint(0,len(name)-1)
+# print (f"The person paying bill will be {name[paying_person]}.")
