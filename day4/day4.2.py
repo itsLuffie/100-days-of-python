1b70fd657dc2c2668fd81b4bd0e1c8987bf24713
Pawan Chaudhary <devrazzchaudhary@proton.me>
2024-04-20 12:56:10 +0545

    random.choice()

--- day4/day4.2.py
+++ day4/day4.2.py
@@ -4,5 +4,7 @@ name = name_string.split(", ")
 import random
 x = random.choice(name)         #choice function will directly pick the name from the function
 print(f"{x} will pay")
+
+# below function can be used to run without choice function
 # paying_person = random.randint(0,len(name)-1)
 # print (f"The person paying bill will be {name[paying_person]}.")
