802de6dc44b19a977d0949868c5c10516e9b8eae
Pawan Chaudhary <devrazzchaudhary@proton.me>
2024-04-20 22:19:49 +0545

    update rock,paper,scissor

--- day4/day4.4.py
+++ day4/day4.4.py
@@ -1,13 +1,12 @@
 #build rock paper scissor
 import random
 
+user = int(input("Enter you decision.\n0. Rock\n1. Paper\n2. Scissor\n"))
 
-user = int(input("Enter you decision.\n1. Rock\n2. Paper\n3. Scissor\n"))
-
-computer = int(random.randint(0,2))
-if computer == user-1:
+computer = random.randint(0,2)
+if user == computer:
     print("It's a tie")
-if computer == user-2 or computer==user+1:
+if user>computer or user ==computer+2:
     print("You win.")
-if computer == user or computer==user+3:
+if user<computer or user == computer-2:
     print("You lose")
