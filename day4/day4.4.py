957e2ee8595b6d4b926c60a0ea3c64f0ef015189
Pawan Chaudhary <devrazzchaudhary@proton.me>
2024-04-21 14:11:20 +0545

    optimized

--- day4/day4.4.py
+++ day4/day4.4.py
@@ -6,7 +6,7 @@ user = int(input("Enter you decision.\n0. Rock\n1. Paper\n2. Scissor\n"))
 computer = random.randint(0,2)
 if user == computer:
     print("It's a tie")
-if user>computer or user ==computer+2:
+elif user>computer or user ==computer+2:
     print("You win.")
-if user<computer or user == computer-2:
+elif user<computer or user == computer-2:
     print("You lose")
