eb928d70778e0e786c1b4e9b95da4d061f476b56
Pawan Chaudhary <devrazzchaudhary@proton.me>
2024-04-21 15:41:20 +0545

    for loop for avg

--- day5/day5.1.py
+++ day5/day5.1.py
@@ -1,7 +1,10 @@
 student_height = input("Enter the heights.").split()
-sum = 0
-for n in range (0, len(student_height)):
-    student_height[n]=int(student_height[n])
-    sum += int(student_height[n])
-avg = sum / (n+1)
-print(avg)
+total_height = 0
+number_of_students=0
+for height in student_height:
+    total_height += int(height)
+for students in student_height:
+    number_of_students += 1
+
+average = (total_height/number_of_students)
+print(average)
