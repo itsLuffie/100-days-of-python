066fa2f0386eaf5477d230d1fe54f97c197a1c41
Pawan Chaudhary <devrazzchaudhary@proton.me>
2024-04-27 20:48:25 +0545

    function definition with argument

--- day8/day8.1.py
+++ day8/day8.1.py
@@ -1,10 +1,19 @@
-import time
-def greet():
+# import time
+# def greet():
+#     print("Welcome to day 8")
+#     time.sleep(1)
+#     print("How you doing")
+#     time.sleep(1)
+#     print("See you around")
+
+
+# greet()
+
+
+def greet_with_name(name):
+    print(f"Hello {name}")
     print("Welcome to day 8")
-    time.sleep(1)
-    print("How you doing")
-    time.sleep(1)
-    print("See you around")
+    
 
 
-greet()
+greet_with_name("Pawan")
