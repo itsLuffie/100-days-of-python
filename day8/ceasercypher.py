621d1e3002f98feb552cb0367dbb67097f5d0cd6
Pawan Chaudhary <devrazzchaudhary@proton.me>
2024-04-29 17:08:45 +0545

    ceaser cypher updated version

--- day8/ceasercypher.py
+++ day8/ceasercypher.py
@@ -1,35 +1,20 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-
-def encrypt(plain_text, shift_amount):
-    cypher_text = ""
-    for letter in plain_text:
+    
+def ceaser(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cypher_text += new_letter
-    print(f"The encoded text is {cypher_text}")
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction}d text is {end_text}")
 
 
-def decrypt(cypher_text, shift_amount):
-    plain_text = ""
-    for letter in cypher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-    print(f"The decoded text is {plain_text}")
-        
-
-if direction == "encode":
-    encrypt(plain_text = text, shift_amount=shift)
-
-
-
-elif direction == "decode":
-    decrypt(cypher_text= text, shift_amount=shift)
+ceaser(start_text=text, shift_amount=shift, cipher_direction=direction)
