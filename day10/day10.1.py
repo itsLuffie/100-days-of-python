945116c8f96936f2b51dc1638c825b074a092168
Pawan Chaudhary <devrazzchaudhary@proton.me>
2024-05-15 11:40:55 +0545

    minor changes

--- day10/day10.1.py
+++ day10/day10.1.py
@@ -1,9 +1,13 @@
 
 
-
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide a valid input."
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
-    return f"{formatted_f_name} {formatted_l_name}"
+    return f"Your name is {formatted_f_name} {formatted_l_name}."
+
+
+
     
-print(format_name("pawan", "chaudhary"))
+print(format_name(input("Enter your first name.\n"), input("Enter your last name.\n")))
