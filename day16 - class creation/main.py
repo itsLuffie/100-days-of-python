7e834a8bbf57a5afa636fb69d303014c0cbe74d3
Pawan Chaudhary <devrazzchaudhary@proton.me>
2024-06-05 22:46:13 +0545

    movement of turtle object

--- day16 - class creation/main.py
+++ day16 - class creation/main.py
@@ -1,9 +1,22 @@
-from turtle import Turtle, Screen
+from turtle import *
 
 timmy = Turtle()
 print(timmy)
 timmy.shape("turtle")
 timmy.color("green")
+
+timmy.setheading(90)
+timmy.heading()
+for i in range(100):
+    timmy.forward(1)
+timmy.left(90)
+for i in range(100):
+    timmy.forward(1)
+timmy.left(90)
+for i in range(100):
+    timmy.forward(1)
+timmy.left(90)
+for i in range(100):
+    timmy.forward(1)
 my_screen = Screen()
-print(my_screen.canvheight)
 my_screen.exitonclick()
