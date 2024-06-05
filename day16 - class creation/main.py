774751b9738dd9649d7a88c1dfe17759d11b86ee
Pawan Chaudhary <devrazzchaudhary@proton.me>
2024-06-05 16:36:28 +0545

    using turtle

---
+++ day16 - class creation/main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen, shape
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
