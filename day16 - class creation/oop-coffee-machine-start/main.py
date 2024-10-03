af997298d0523877362b1e0686e72f426e01ba41
itsLuffie <coding.uncounted729@passmail.com>
2024-10-03 21:05:59 +0545

    update main.py

--- day16 - class creation/oop-coffee-machine-start/main.py
+++ day16 - class creation/oop-coffee-machine-start/main.py
@@ -1,3 +1,6 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+choice = input("What do you like?\n1. Espresso\n2. Cappuccino\n3. Latte\n")
