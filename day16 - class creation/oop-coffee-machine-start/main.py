9d21384f82a93fd9c61c6b62f5f390f479175089
itsLuffie <coding.uncounted729@passmail.com>
2024-10-07 20:15:07 +0545

    coffee machine completed

--- day16 - class creation/oop-coffee-machine-start/main.py
+++ day16 - class creation/oop-coffee-machine-start/main.py
@@ -3,4 +3,23 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
-choice = input("What do you like?\n1. Espresso\n2. Cappuccino\n3. Latte\n")
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+is_on = True
+menu = Menu()
+
+
+
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
